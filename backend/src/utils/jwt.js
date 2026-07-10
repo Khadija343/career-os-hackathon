05b934c06c8407090b42d4a8e0c67a93710355a8
Khadija Khalid <ksaeed191@gmail.com>
2026-07-11 02:11:16 +0500

    Polished Version

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -4,8 +4,6 @@ import jwt from "jsonwebtoken";
  * Generate JWT Access Token
  */
 export const generateAccessToken = (payload) => {
-  console.log("JWT_EXPIRES_IN =", JSON.stringify(process.env.JWT_EXPIRES_IN));
-  console.log("JWT_SECRET exists =", !!process.env.JWT_SECRET);
   return jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
